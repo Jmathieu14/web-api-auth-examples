628baae9072de7c918d53c1021d58d0467964616
Jacques Mathieu <jmathfb@gmail.com>
2019-03-07 16:28:17 -0500

    Updated with client id + secret

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '5d136501dbca45f8b7069346b31ae8ee'; // Your client id
+var client_secret = 'd649ee0bb8bc46a3aed35c52619f862f'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
